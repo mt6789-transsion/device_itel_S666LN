3a67e21718d76c6d81e9ba4241dbef5643a85282
fjrXTR <fajarslebew31@gmail.com>
2025-07-16 06:12:34 +0200

    S666LN: Import AIDL vibrator services
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- device.mk
+++ device.mk
@@ -454,7 +454,7 @@ $(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator-service.mediatek
+    android.hardware.vibrator-service.s666ln
 
 # VNDK
 PRODUCT_PACKAGES += \
